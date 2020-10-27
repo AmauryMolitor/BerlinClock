9ae08d2529b4c4808a90088dd69db1c842cf380b
Amaury Molitor <amaury.molitor@student.vinci.be>
2020-10-27 19:31:02 +0100

    add - first tests

--- TestBerlinClock.php
+++ TestBerlinClock.php
@@ -4,4 +4,11 @@
 class TestBerlinClock
 {
 
+    public function testSeconds() {
+        assertEquals("Y", berlinClock.getSeconds(0));
+        assertEquals("O", berlinClock.getSeconds(1));
+        assertEquals("Y", berlinClock.getSeconds(42));
+        assertEquals("O", berlinClock.getSeconds(59));
+    }
+
 }
